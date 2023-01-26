623675165f60eb55e6a47e29e599a687a1b27e94
Renato Alves Soares <renatoguara2020@gmail.com>
2023-01-26 20:18:08 -0300

    ok

--- delete.php
+++ delete.php
@@ -1,15 +1,15 @@
 <?php
 //including the database connection file
-include("config.php");
+include 'config.php';
 
 //getting id of the data from url
 $id = $_GET['id'];
 
 //deleting the row from table
-$sql = "DELETE FROM users WHERE id=:id";
-$query = $dbConn->prepare($sql);
-$query->execute(array(':id' => $id));
+$sql = 'DELETE FROM users WHERE id=:id';
+$query = $conn->prepare($sql);
+$query->execute([':id' => $id]);
 
 //redirecting to the display page (index.php in our case)
-header("Location:index.php");
+header('Location:index.php');
 ?>
